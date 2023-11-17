fe50069edd99722666e113437e91f7ce24804a5b
Tharindra <tharindragayashika@gmail.com>
2023-11-17 11:56:36 +0530

    University filter added to event main page

--- Frontend-Tharindra/events/events-index/events-index-js.js
+++ Frontend-Tharindra/events/events-index/events-index-js.js
@@ -1,5 +1,44 @@
 const uniFilter =document.querySelector(".uni-filter"),
-selectBtn = uniFilter.querySelector(".select-btn");
+selectBtn = uniFilter.querySelector(".select-btn"),
+searchInput = uniFilter.querySelector("input"),
+uniFilterOptions= uniFilter.querySelector(".uni-filter-options");
+
+
+let universities = ["Colombo","Moratuwa", "Jaffna", "SLIIT", "IIT", "UWU", "KDU"];
+
+
+function addUniversity(selectedUniversity){
+    uniFilterOptions.innerHTML= "";
+    universities.forEach(university=> {
+        //if selected university and university value is asame then add selected class
+        let isSelected = university == selectedUniversity ? "selected" : "";
+        //adding each university inside li and inserting all li inside uni-filter-options
+        let li = `<li onclick="updateName(this)" class="${isSelected}"> ${university} </li>`;
+        uniFilterOptions.insertAdjacentHTML("beforeend", li);
+    });
+}
+addUniversity();
+
+function updateName(selectedLi){
+    searchInput.value= "";
+    addUniversity(selectedLi.innerText);
+    uniFilter.classList.remove("uni-filter-active");
+    selectBtn.firstElementChild.innerText = selectedLi.innerText;
+}
+searchInput.addEventListener("keyup", ()=>{
+    // console.log(searchInput.value);
+    let arr =[]; //creating empty array
+    let searchedVal= searchInput.value.toLowerCase();
+    //returning all universities from array which are start with user search value
+    //and mapping returned university with li and joining them
+    arr = universities.filter(data=>{
+        return data.toLowerCase().startsWith(searchedVal);
+    }).map(data=>
+        `<li onclick="updateName(this)" > ${data} </li>`
+    ).join("");
+    console.log(arr);
+    uniFilterOptions.innerHTML = arr ? arr : `<p>Oops! University not found</p>`;
+})
 
 selectBtn.addEventListener("click", () => {
     uniFilter.classList.toggle("uni-filter-active");
