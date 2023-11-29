c96e872095fcdc0230592255b4daddc19db7f0be
Tharindra <tharindragayashika@gmail.com>
2023-11-30 01:11:17 +0530

    did some changes on backend

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,13 +1,16 @@
+
+<!-- from-here -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> <?php echo SITENAME; ?> </title>
-    <link rel="stylesheet" href="<?php echo URLROOT?>/css/style.css">
+    <title>Events</title>
+    <link rel="stylesheet" href="<?php echo URLROOT?>/css/header_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <?php require APPROOT . '/views/inc/navbar.php'; ?>
-    <div class="border-container">
+        
+<!-- from-here -->
 
     
