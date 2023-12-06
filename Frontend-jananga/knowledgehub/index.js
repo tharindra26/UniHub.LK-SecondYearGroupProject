1f6c83800030241f7c9930987fa55c62e8e54b80
Jananga Wijewardhana <kavinduwijewardhana1999@gmail.com>
2023-12-06 23:50:30 +0530

    modified post box

---
+++ Frontend-jananga/knowledgehub/index.js
@@ -0,0 +1,10 @@
+function createCard() {
+    var parent = document.getElementById("card_parent");
+    let card = document.createElement("div");
+    card.className = "card";
+
+    parent.appendChild(card);
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+});
