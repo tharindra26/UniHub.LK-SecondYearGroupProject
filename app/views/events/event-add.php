8c4ed59dd5eb60e3dcca4d34d6e7d5096173932e
Tharindra <tharindragayashika@gmail.com>
2023-12-19 02:47:31 +0530

    event add v1.0

---
+++ app/views/events/event-add.php
@@ -0,0 +1,34 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<?php require APPROOT . '/views/inc/navbar.php'; ?>
+<link rel="stylesheet" href="<?php echo URLROOT ?>/css/events/event-add_style.css">
+
+<div class="container">
+    <div class="inner-container">
+        <div class="add-event-form">
+            <header>Add Event</header>
+            <form action="" class="form">
+                <div class="input-box">
+                    <label for="">Full Name</label>
+                    <input type="text" name="" id="" placeholder="Enter full name">
+                </div>
+
+                <div class="input-box">
+                    <label for="">Full Name</label>
+                    <input type="text" name="" id="" placeholder="Enter full name">
+                </div>
+            </form>
+        </div>
+
+
+        <div class="image-section">
+            <img src="<?php echo URLROOT ?>/img/events/event-add/event-add-image.jpg" alt="" srcset="">
+        </div>
+    </div>
+    
+    
+</div>
+
+
+
+<script src="<?php echo URLROOT?>/js/events/event-add.js"></script>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
