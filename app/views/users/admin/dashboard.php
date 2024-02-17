3c7eded6eec27b8be15e38dd08bfd1663f65b6ae
Viruli Vimansa Weerasinghe <2021cs214@stu.ucsc.cmb.ac.lk>
2024-02-17 21:17:06 +0530

    sidebar-update

--- app/views/users/admin/dashboard.php
+++ app/views/users/admin/dashboard.php
@@ -1,19 +1,61 @@
 
 <link rel="stylesheet" href="<?php echo URLROOT ?>/css/users/admin/adminprofile_style.css">
+<link rel="stylesheet" href="<?php echo URLROOT ?>/css/users/admin/dashboard_style.css">
 
 <h1 class="section-title">Dashboard</h1>
 
 <div class="dashbord-area">
-    <div class="chart-row">
+    <div class="summary">
+        <div class="box" >
+            <div class="summary-content">
+                <span class="num" data-val="2240">0000</span>
+                <h3>Users</h3>
+            </div>
+            <div class="icons" ><i class="fa-solid fa-users"></i></div>   
+        </div>
+
+        <div class="box" >
+            <div class="summary-content">
+                <span class="num" data-val="2240">0000</span>
+                <h3>Events</h3>
+            </div>
+            <div class="icons" ><i class="fa-solid fa-calendar-week"></i></div> 
+         </div>
+
+        <div class="box" >
+            <div class="summary-content">
+                <span class="num" data-val="2240">0000</span>
+                <h3>Organizations</h3>
+            </div>
+            <div class="icons" ><i class="fa-solid fa-sitemap"></i></div> 
+         </div>
+
+        <div class="box" >
+            <div class="summary-content">
+                <span class="num" data-val="2240">0000</span>
+                <h3>Posts</h3>
+            </div>
+            <div class="icons" ><i class="fa-solid fa-file-signature"></i></div> 
+         </div>
+
+        <div class="box" >
+            <div class="summary-content">
+                <span class="num" data-val="2240">0000</span>
+                <h3>Opportunities</h3>
+            </div>
+            <div class="icons" ><i class="fa-solid fa-school"></i></div> 
+         </div>
+    </div>
+    <!-- <div class="chart-row">
         <div class="chart-section">
             <h3>Organizations</h3>
             <canvas id="chart1"></canvas>
         </div>
         <div class="chart-section"><canvas id="chart2"></canvas></div>
-    </div>
+    </div> -->
     <div class="chart-row">
-        <div class="chart-section"><canvas id="chart3"></canvas></div>
-        <div class="chart-section"><canvas id="chart4"></canvas></div>
+        <div class="chart-section"><canvas id="chart1"></canvas></div>
+        <div class="chart-section"><canvas id="chart2"></canvas></div>
     </div>
 
 </div>
