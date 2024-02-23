b797487a453da1b80d880b1a0cbc88bd5bb93ae2
Tharindra <tharindragayashika@gmail.com>
2024-02-23 23:41:39 +0530

    event reviews update

--- app/views/users/undergraduate/showEducation.php
+++ app/views/users/undergraduate/showEducation.php
@@ -21,18 +21,25 @@
 
             <div class="left-box">
                 <div class="form-outer-box">
-                        <div class="row">
-                            <?php if (!empty($data['education'][0]->education_id)) : ?> 
-                                <?php foreach ($data['education'] as $education) : ?>
-                                    <?php $edu_id = $education->education_id; ?>
+                    <div class="row">
+                        <?php if (!empty($data['education'][0]->education_id)): ?>
+                            <?php foreach ($data['education'] as $education): ?>
+                                <?php $edu_id = $education->education_id; ?>
                                 <div class="content">
                                     <div class="edu-info">
-                                        <h4><?php echo $education->institution ?></h4>
-                                        <p><?php echo $education->start_year?> - <?php echo $education->end_year?></p>
-                                        <p><?php echo $education->description ?></p>
+                                        <h4>
+                                            <?php echo $education->institution ?>
+                                        </h4>
+                                        <p>
+                                            <?php echo $education->start_year ?> -
+                                            <?php echo $education->end_year ?>
+                                        </p>
+                                        <p>
+                                            <?php echo $education->description ?>
+                                        </p>
                                     </div>
                                     <div class="edu-btn">
-                                        <button onclick="openPopup('<?php echo $edu_id; ?>')">Update</button>
+                                        <button onclick="openPopup(<?php echo $edu_id; ?>)">Update</button>
                                         <!-- popupModal -->
                                         <span class="overlay"></span>
                                         <div class="modal-box" id="<?php echo $edu_id; ?>">
@@ -41,63 +48,72 @@
                                             <!-- <h3>Please check your email and password and try again.</h3> -->
                                             <!-- Form Starts -->
                                             <div class="form-outer-box">
-                        <form class="form" action="<?php echo URLROOT; ?>/users/editEducation/<?php echo $data['education_id'] ?>" method="post"
-                        enctype="multipart/form-data">
+                                                <form class="form"
+                                                    action="<?php echo URLROOT; ?>/users/editEducation/<?php echo $data['education_id'] ?>"
+                                                    method="post" enctype="multipart/form-data">
 
-                        <div class="column">
-                            <div class="input-box">
-                                <label for="">Institute</label>
-                                <input type="text" name="institution"  id="" placeholder="Enter the Institution" value="<?php echo $data['institution'] ?>">
-                                <!--  <?php if (!empty($data['institution_err'])): ?>
+                                                    <div class="column">
+                                                        <div class="input-box">
+                                                            <label for="">Institute</label>
+                                                            <input type="text" name="institution" id=""
+                                                                placeholder="Enter the Institution"
+                                                                value="<?php echo $data['institution'] ?>">
+                                                            <!--  <?php if (!empty($data['institution_err'])): ?>
                                     <span class="error-message"><?php echo $data['institution_err']; ?></span>
                                 <?php endif; ?> -->
-                            </div>
-                        </div>
+                                                        </div>
+                                                    </div>
 
-                        <div class="column">
-                            <div class="input-box">
-                                <label for="">Description</label>
-                                <input type="text" name="description"  id="" placeholder="Enter the Description" value="<?php echo $data['description'] ?>">
-                                <!-- <?php if (!empty($data['description_err'])): ?>
+                                                    <div class="column">
+                                                        <div class="input-box">
+                                                            <label for="">Description</label>
+                                                            <input type="text" name="description" id=""
+                                                                placeholder="Enter the Description"
+                                                                value="<?php echo $data['description'] ?>">
+                                                            <!-- <?php if (!empty($data['description_err'])): ?>
                                     <span class="error-message"><?php echo $data['description_err']; ?></span>
                                 <?php endif; ?> -->
-                            </div>
-                        </div>
+                                                        </div>
+                                                    </div>
 
-                        <div class="column">
-                            <div class="input-box">
-                                <label for="">Start Year</label>
-                                <input type="text" name="start_year"  id="" placeholder="Enter the Start Year" value="<?php echo $data['start_year'] ?>">
-                                <!-- <?php if (!empty($data['start_year_err'])): ?>
+                                                    <div class="column">
+                                                        <div class="input-box">
+                                                            <label for="">Start Year</label>
+                                                            <input type="text" name="start_year" id=""
+                                                                placeholder="Enter the Start Year"
+                                                                value="<?php echo $data['start_year'] ?>">
+                                                            <!-- <?php if (!empty($data['start_year_err'])): ?>
                                     <span class="error-message"><?php echo $data['web_err']; ?></span>
                                 <?php endif; ?> -->
-                            </div>
-                            <div class="input-box">
-                                <label for="">End Year</label>
-                                <input type="tel" name="end_year"  id="" placeholder="Enter the End Year" value="<?php echo $data['end_year'] ?>">
-                                <!-- <?php if (!empty($data['end_year_err'])): ?>
+                                                        </div>
+                                                        <div class="input-box">
+                                                            <label for="">End Year</label>
+                                                            <input type="tel" name="end_year" id=""
+                                                                placeholder="Enter the End Year"
+                                                                value="<?php echo $data['end_year'] ?>">
+                                                            <!-- <?php if (!empty($data['end_year_err'])): ?>
                                     <span class="error-message"><?php echo $data['end_year_err']; ?></span>
                                 <?php endif; ?> -->
-                            </div>
-                        </div>
+                                                        </div>
+                                                    </div>
 
-                        <button class="submit-btn" type="submit">Update</button>
-                    </form>
-                </div>
+                                                    <button class="submit-btn" type="submit">Update</button>
+                                                </form>
+                                            </div>
 
-                <!-- rating-system -->
+                                            <!-- rating-system -->
 
 
-            </div>
+                                        </div>
 
-            <!-- popupModal -->
+                                        <!-- popupModal -->
 
                                         <button>Delete</button>
                                     </div>
                                 </div>
-                                <?php endforeach; ?>
-                            <?php endif; ?>
-                        </div>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
 
@@ -109,6 +125,8 @@
 </div>
 
 
+
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-<script src="<?php echo URLROOT ?>/js/users/undegraduate/showEducation.js"></script>
+<script src="<?php echo URLROOT ?>/js/users/undergraduate/showEducation.js"></script>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
