bfd4ad9b3b74e1ac258cfed1265e125146a2dfed
Viruli Vimansa Weerasinghe <2021cs214@stu.ucsc.cmb.ac.lk>
2024-02-23 23:35:54 +0530

    user-education delete

--- public/js/users/undergraduate/showEducation.js
+++ public/js/users/undergraduate/showEducation.js
@@ -21,3 +21,62 @@ overlay.addEventListener("click", () => {
   overlay.classList.remove("active");
 });
 //popup
+
+function updateEducation(edu_id) {
+    let element = document.getElementById(edu_id);
+    let overlay = document.querySelector(".overlay");
+    // // Your AJAX function here
+    $.ajax({
+      url: "http://localhost/unihub/users/updateEducation",
+      type: "POST", // or 'GET' depending on your needs
+      data: {
+        education_id: edu_id,
+      },
+      success: function (response) {
+        // Handle the success response
+        console.log("AJAX request successful:", response);
+            element.classList.add("active");
+            overlay.classList.add("active");
+        
+      },
+      error: function (error) {
+        // Handle the error response
+        console.error("AJAX request failed:", error);
+
+      },
+    });
+    // if (popup) popup.classList.remove("open-popup");
+  }
+
+  function confirmDelete(edu_id) {
+    // // Your AJAX function here
+    $.ajax({
+      url: "http://localhost/unihub/users/deleteEducation",
+      type: "POST", // or 'GET' depending on your needs
+      data: {
+        education_id: edu_id,
+      },
+      success: function (response) {
+        // Handle the success response
+        console.log("AJAX request successful:", response);
+  
+        // Update the text content on success
+        // interestedBtn.find('span').text(response);
+        if (response === "1") {
+            let element = document.getElementById(edu_id);
+            if (element) {
+                element.classList.remove("active"); 
+                overlay.classList.remove("active");
+            }
+        }
+        //else {
+        //     confirmBtn.addClass("new-class");
+        // }
+      },
+      error: function (error) {
+        // Handle the error response
+        console.error("AJAX request failed:", error);
+      },
+    });
+    // if (popup) popup.classList.remove("open-popup");
+  }
