7e8cae46e3dd3f55630685ae6564c08a84150ed3
Viruli Vimansa Weerasinghe <2021cs214@stu.ucsc.cmb.ac.lk>
2024-04-11 09:04:43 +0530

    update profile photo

--- app/views/users/undergraduate/searchUser.php
+++ app/views/users/undergraduate/searchUser.php
@@ -2,12 +2,14 @@
 <span class="overlay-search"></span>
 <?php if (!empty($data['users'][0]->id)) : ?> 
     <?php foreach ($data['users'] as $users) : ?>
+        <a href="<?php echo URLROOT ?>/users/show/<?php echo $users->id ?>" class="profile-link">
         <div class="search-results">
             <div class="profile-pic">
                 <img src="<?php echo URLROOT ?>/img/users/users_profile_images/<?php echo $users->profile_image ?>" alt="">
             </div>
-            <a href="#"><?php echo $users->fname , " " , $users->lname ?></a>
+            <div class="profile-name"><?php echo $users->fname , " " , $users->lname ?></div>
         </div> 
+        </a>
         <hr>
     <?php endforeach; ?>
     <?php else: ?><div class="search-results"><p>No result found</p></div><hr>
