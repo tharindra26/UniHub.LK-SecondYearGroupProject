6c6e7e28427428590d7f977889042f06dbae031a
Tharindra <tharindragayashika@gmail.com>
2024-04-30 08:31:27 +0530

    final touch v4

--- app/models/Post.php
+++ app/models/Post.php
@@ -859,6 +859,7 @@ class Post
         p.*,
         u.fname,
         u.lname,
+        u.profile_image AS user_profile_image,
         GROUP_CONCAT(DISTINCT pt.tag_text) AS tags,
         GROUP_CONCAT(DISTINCT pc.category_name) AS categories,
         GROUP_CONCAT(DISTINCT pl.user_id) AS liked_users,
